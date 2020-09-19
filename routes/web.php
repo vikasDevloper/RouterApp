7c1bdfdff9bf208c6ee75340f99dd3fe5fbba103
vikas <vikasdubey261@gmail.com>
2020-09-19 18:26:17 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', function () {
-    return view('welcome');
+    return view('welcome'); 
 });
 Route::resource('ajaxproducts','ProductAjaxController');
